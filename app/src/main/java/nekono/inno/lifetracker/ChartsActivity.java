2cddab6ed6c23f3dc8c4fd92c31a38dfc50b4a04
Егор Ивков <e.o.ivkov@gmail.com>
2018-04-15 18:32:23 +0300

    Design corrections

--- app/src/main/java/nekono/inno/lifetracker/ChartsActivity.java
+++ app/src/main/java/nekono/inno/lifetracker/ChartsActivity.java
@@ -75,15 +75,6 @@ public class ChartsActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
